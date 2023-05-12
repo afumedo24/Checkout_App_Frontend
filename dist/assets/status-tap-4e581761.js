f31593f3a6c85114f33f01505a3267c51de75f9c
ahmed <abdaal10icloud.com>
2023-05-13 01:18:15 +0200

    trying to implement the qr-code scanner

--- dist/assets/status-tap-4e581761.js
+++
@@ -1,3 +0,0 @@
-import{a,b as i,d}from"./index-43ac3e3c.js";import{f as c,s as l}from"./index8-ac70b054.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const n=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(n/2,s/2);if(!o)return;const t=c(o);t&&new Promise(r=>i(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
