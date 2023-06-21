2c5c715297a0358099278d7b12157d69db87cbe2
Rusu <viniciusrusu05@gmail.com>
2023-06-21 17:27:32 +0200

    UserPage und AdminPage

---
+++ src/views/UserPage2.vue
@@ -0,0 +1,147 @@
+<template>
+    <ion-page>
+        <ion-header> 
+            <app-header> </app-header>
+        </ion-header>
+        <ion-content >
+                <ion-card class="user-list">
+                    <ion-card-header class="title" >
+                        <ion-card-title >Zum Konto Einlogen</ion-card-title>
+                        <ion-card-subtitle style="color: red">Bitte Überprüfen Sie Ihre Daten!</ion-card-subtitle>
+                    </ion-card-header>
+                   
+  
+                    <ion-card-content>
+                        <ion-list  class="user-item" >
+                            <ion-item >
+                            <ion-input v-model="user" labelPlacement="standard" placeholder="User" ></ion-input>
+                            </ion-item>
+                        </ion-list>
+                        
+  
+                    </ion-card-content>
+                    <ion-card-content>
+                        <ion-list class="user-item">
+  
+  
+                            <ion-item >
+                            <ion-select label="Geräte" label-placement="floating" placeholder="Geräte auswählen">
+                                <ion-select-option value="">No Game Console</ion-select-option>
+                                <ion-select-option value="nes">NES</ion-select-option>
+                                <ion-select-option value="n64">Nintendo64</ion-select-option>
+                                <ion-select-option value="ps">PlayStation</ion-select-option>
+                                <ion-select-option value="genesis">Sega Genesis</ion-select-option>
+                                <ion-select-option value="saturn">Sega Saturn</ion-select-option>
+                                <ion-select-option value="snes">SNES</ion-select-option>
+                            </ion-select>
+                            </ion-item>
+  
+  
+                            
+                            
+                            
+                            <!--ion-datetime slot="start"></ion-datetime>
+                        <ion-datetime presentation="month-year"></ion-datetime>
+                            <ion-datetime slot="end"></ion-datetime-->
+                           
+  
+                        </ion-list>
+                    </ion-card-content>
+                    <ion-card-content >
+                        
+                        <ion-button v-on:click="signUp" class="borrow-button"> Senden </ion-button>
+                    </ion-card-content>
+                </ion-card>
+            <!--ion-datetime></ion-datetime-->
+        </ion-content>
+      </ion-page>
+  </template>
+  
+  <script>
+  import { IonContent, IonPage,IonModal, IonHeader,IonDatetime, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput, IonItem, IonList, IonPopover, IonAccordion, IonNote, IonAccordionGroup} from '@ionic/vue';
+  import AppHeader from '../components/Header.vue';
+  import axios from 'axios';
+  
+  export default {
+  components: {
+    AppHeader, IonPage, IonContent, IonModal, IonHeader, IonDatetime, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonInput, IonItem, IonList, IonPopover, IonAccordion, IonNote, IonAccordionGroup
+  },
+     
+      data()
+      {
+          return{
+              user:'',
+          }
+      },
+      methods:{
+      async signUp()
+      {
+          let result = await axios.get("http://localhost:8300/api/users/",{
+              user:this.user
+  
+          });
+          console.warn(result)    
+      }
+  }
+  };
+  
+  
+  </script>
+  
+  <style scoped>
+  .user-list {
+    display: block;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    width: 60%;
+    margin-top: 10%;
+    margin-left: 20%;
+    background-color: #6a6a6a;
+    border-radius: 20px;
+    text-align: center;
+    
+  }
+  .user-item {
+    padding: 10px;
+    display: block;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    width: 90%;
+    margin-top: 5%;
+    margin-left: 5%;
+    color: #D9D9D9;
+    border-radius: 20px;
+   
+  }
+  
+  .device-item img {
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+  
+  .status-green{
+  color: var(--ion-color-success);
+  }
+  .status-red{
+    color: var(--ion-color-danger);
+  }
+  .borrow-button{
+      
+    --background: var( --ion-color-secondary-shade);
+    height: 4.5vh;
+    width: 9.5vh;
+  }
+  .title{
+    font-size: 28px;
+    font-weight: bolder;
+    margin-left: 10px;
+  }
+  
+  
+  
+  </style>
+  
+  
